9570a7d999bc5fc4e345a3393d5d58f0dfc06387
rewatbu <rewatbu@gmail.com>
2026-08-20 14:32:28 +0700

    add todos objects

--- react-builday/src/App.jsx
+++ react-builday/src/App.jsx
@@ -6,13 +6,18 @@ import TodoList from "./components/TodoList";
 
 
 function App() {
+  const todos = [
+    { id: 1, task: "Learn React" },
+    { id: 2, task: "Practice JavaScript" },
+    { id: 3, task: "Build Todo App" },
+  ]
 
   return (
     <div>
       <h1>To Do List</h1>
       <TodoForm />
       
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   )
 }
