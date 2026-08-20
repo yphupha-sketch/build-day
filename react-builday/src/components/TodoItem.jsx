e4aaf770c727d66b322dd91a6d283a00ec08b36b
GeorgeBush <wps.wisit@gmail.com>
2026-08-20 13:52:59 +0700

    form1

---
+++ react-builday/src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+function TodoItem() {
+  return (
+    <div>
+      <input type="checkbox" />
+
+      <span>Learn React</span>
+
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;
